23e869920bdf21c028a4dd73d133ea454a452def
RhyanSilva <77513586+RhyanSilva@users.noreply.github.com>
2021-01-15 19:12:29 -0300

    Add files via upload

---
+++ Coleta_Informações.py.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+from socket import *
+import socket
+import whois
+
+
+def scan(dominios):
+    try:
+        dominio = dominios
+        s_whois = whois.whois(dominio)
+        print (s_whois.text)
+        arquivo = open(dominios + '_whois.txt', 'w')
+        arquivo.write(s_whois.text)
+    except:
+        pass
+
+p=scan(input("Qual site para o Whois ? "))
+
+
+while True:
+    rb = int(input("\n0-Sair\n1-Ip do site\n\n>>"))
+    if(rb==0):
+        print("Saindo e entrando no Scan !")
+        break
+    else:
+        vit= str(input("Site: "))
+        p=gethostbyname(vit)
+        print("\nIP do site é: ",p)
+
+
+
+host=input("Ip do Alvo: ")
+for porta in range(20, 100):
+    net=socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    net.settimeout(1)
+    resultado=net.connect_ex((host, porta))
+    net.close()
+    if resultado == 0:
+        print(f'Portas abertas: {porta}')
+
+
+print('Script by : Silva')
+
+
+
+
+
+
+
