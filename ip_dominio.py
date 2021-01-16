e2173961d5a8c58fb592fa2e11ddce86dfd1839b
RhyanSilva <77513586+RhyanSilva@users.noreply.github.com>
2021-01-16 16:05:10 -0300

    Update and rename Ip_dominio.py to ip_dominio.py

--- ip_dominio.py
+++ ip_dominio.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#You can change the name this file , and Start with code : python <name.py> or python ip_dominio.py
 from socket import *
 while True:
     rb = int(input("\n0-Sair\n1-Ip Dominio\n\n>>"))
