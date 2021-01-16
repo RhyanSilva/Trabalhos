82d66a833adcee241820dde8ee8d107ab0d010a5
RhyanSilva <77513586+RhyanSilva@users.noreply.github.com>
2021-01-16 16:04:28 -0300

    Update and rename Scan.py to scan.py

--- scan.py
+++ scan.py
@@ -1,3 +1,5 @@
+#usr/bin/python
+#You can change the name this file , and Start with code : python <name.py> or python scan.py
 import socket
 host=input("Ip do Alvo: ")
 for porta in range(20, 1000):
